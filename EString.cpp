2ae075c1a49485eacd3aed17c626a41320e2aa84
arseny.saw4enko@gmail.com <arseny.saw4enko@gmail.com>
2023-05-02 17:06:42 +0300

    Merging sources to single file, tests are temporary removed

--- EString.cpp
+++ EString.cpp
@@ -1,6 +1,46 @@
 #include "EString.h"
 
+#ifdef _WIN32
 #include <Windows.h>
+#endif
+
+static void decode_data_from_console(std::string const& buffer, String& out_str);
+
+std::basic_istream<char, std::char_traits<char>>& operator>>(std::basic_istream<char, std::char_traits<char>>& stream, String& out_str) {
+  std::string buffer;
+  stream >> buffer;
+
+  decode_data_from_console(buffer, out_str);
+
+  return stream;
+}
+
+#ifdef _WIN32
+
+static void decode_data_from_console(std::string const& buffer, String& out_str) {
+  unsigned int console_code_page = GetConsoleCP();
+
+  wchar_t* wide_buffer = new wchar_t[buffer.size()];
+
+  int return_value = MultiByteToWideChar(
+    console_code_page, 0,
+    buffer.c_str(), buffer.size(),
+    wide_buffer, buffer.size()
+  );
+
+  if (return_value == 0) {
+    throw encoding_failed("console", "Failed to decode string from console");
+  }
+
+  out_str.decode<wchar_t>(wide_buffer, return_value);
+
+  delete[] wide_buffer;
+}
+
+#endif
+
+// ANSI encoding
+#ifdef _WIN32 
 
 using size_type = typename AnsiEncodingTraits::size_type;
 using encoded_char_type = typename AnsiEncodingTraits::encoded_char_type;
@@ -28,3 +68,5 @@ size_type AnsiEncodingTraits::char_length(const encoded_char_type* encoded_char)
 size_type AnsiEncodingTraits::str_length(const encoded_char_type* string) {
   throw encoding_failed(AnsiEncodingTraits::encoding_name, "Ansi not implimented.");
 }
+
+#endif
--- EString.h
+++ EString.h
@@ -16,9 +16,7 @@ public:
   constexpr String() = default;
 
   constexpr String(const char32_t* utf32_string) : String() {
-    // Count string size
-    size_type string_size;
-    for (string_size = 0; utf32_string[string_size]; ++string_size);
+    size_type string_size = Utf32EncodingTraits::str_length(utf32_string);
 
     _construct_with_string_and_size(utf32_string, string_size);
   }
@@ -32,17 +30,47 @@ public:
     decode(encoded_string, encoded_string_length_in_chars);
   }
 
+  constexpr String(size_type count, char32_t character = 0) : String() {
+    _need_allocated(count);
+
+    for (size_type i = 0; i < count; ++i)
+      m_buffer[i] = character;
+  }
+
   template <typename CharType>
   constexpr String(const CharType* encoded_string) : String(encoded_string, EncodingTraits<CharType>::str_length(encoded_string)) {}
 
   template <typename CharType>
   constexpr String(std::basic_string<CharType, std::char_traits<CharType>> const& encoded_string) : String(encoded_string.c_str(), encoded_string.length()) {}
 
+  constexpr String(String const& other) : String() {
+    _need_allocated(other.m_length + 1);
+
+    for (size_type i = 0; i < other.m_length; ++i)
+      m_buffer[i] = other.m_buffer[i];
+
+    m_buffer[other.m_length] = 0;
+  }
+
+  constexpr String(String&& other) {
+    m_buffer = other.m_buffer;
+    m_length = other.m_length;
+    m_allocated = other.m_allocated;
+    other.m_buffer = nullptr;
+    other.m_length = 0;
+    other.m_allocated = 0;
+  }
+
 public:
   template <typename CharType>
   constexpr std::basic_string<CharType, std::char_traits<CharType>, std::allocator<CharType>> encode() const {
     using encoding_traits = EncodingTraits<CharType>;
     using string_type = std::basic_string<CharType, std::char_traits<CharType>, std::allocator<CharType>>;
+
+    if constexpr (std::is_same_v<CharType, char32_t>) {
+      // Optimization
+      return string_type(m_buffer, m_length);
+    }
 
     if (m_length == 0)
       return string_type();
@@ -78,6 +106,128 @@ public:
   }
 
 public:
+  constexpr char32_t& front() {
+    return m_buffer[0];
+  }
+
+  constexpr char32_t front() const {
+    return m_buffer[0];
+  }
+
+  constexpr char32_t& back() {
+    return m_buffer[m_length - 1];
+  }
+
+  constexpr char32_t back() const {
+    return m_buffer[m_length - 1];
+  }
+
+  constexpr char32_t* data() {
+    return m_buffer;
+  }
+
+  constexpr const char32_t* data() const {
+    return m_buffer;
+  }
+
+  constexpr char32_t* begin() {
+    return m_buffer;
+  }
+
+  constexpr const char32_t* begin() const {
+    return m_buffer;
+  }
+
+  constexpr const char32_t* cbegin() const {
+    return m_buffer;
+  }
+
+  constexpr char32_t* end() {
+    return m_buffer;
+  }
+
+  constexpr const char32_t* end() const {
+    return m_buffer;
+  }
+
+  constexpr const char32_t* cend() const {
+    return m_buffer;
+  }
+
+  constexpr bool is_empty() const {
+    return !m_buffer || m_length == 0;
+  }
+
+  constexpr size_type length() const {
+    return m_length;
+  }
+
+  constexpr size_type size() const {
+    return m_length;
+  }
+
+  constexpr size_type max_size() const {
+    return static_cast<size_type>(~0) / sizeof(char32_t);
+  }
+
+  constexpr void reserve(size_type count) {
+    _need_allocated(count);
+  }
+
+  constexpr size_type capacity() const {
+    return m_allocated;
+  }
+
+  constexpr void shrink_to_fit() {
+    if (m_allocated > (m_length + 1)) {
+      _reallocate(m_length + 1);
+    }
+  }
+
+  constexpr void clear() {
+    m_buffer[0] = 0;
+    m_length = 0;
+  }
+
+  constexpr String& insert(size_type index, size_type count, char32_t character) {
+    _need_allocated(m_length + count + 1);
+    _move_right(index, count);
+
+    for (size_type i = 0; i < count; ++i, ++index)
+      m_buffer[index] = character;
+    m_length = index;
+
+    return *this;
+  }
+
+  constexpr String& insert(size_type index, const char32_t* string) {
+    return insert(index, string, Utf32EncodingTraits::str_length(string));
+  }
+
+  constexpr String& insert(size_type index, const char32_t* string, size_type string_length_in_characters) {
+    _need_allocated(m_length + string_length_in_characters + 1);
+    _move_right(index, string_length_in_characters);
+
+    for (size_type i = 0; i < string_length_in_characters; ++i, ++index)
+      m_buffer[index] = string[i];
+    m_length = index;
+
+    return *this;
+  }
+
+  constexpr String& insert(size_type index, String const& string) {
+    return insert(index, string.m_buffer, string.m_length);
+  }
+
+  constexpr String& erase(size_type index, size_type count) {
+    _move_left(index, count);
+    m_length = index + count;
+    m_buffer[m_length] = 0;
+
+    return *this;
+  }
+
+public:
   template <typename CharType>
   constexpr String& operator=(const CharType* encoded_string) {
     decode(encoded_string);
@@ -88,6 +238,20 @@ public:
   constexpr String& operator=(std::basic_string<CharType, std::char_traits<CharType>> const& encoded_string) {
     decode(encoded_string);
     return *this;
+  }
+
+  constexpr char32_t& operator[](size_type index) {
+    return m_buffer[index];
+  }
+
+  constexpr char32_t operator[](size_type index) const {
+    return m_buffer[index];
+  }
+
+  // Cast to STL string.
+  template <typename CharType>
+  constexpr operator std::basic_string<CharType, std::char_traits<CharType>, std::allocator<CharType>>() {
+    return encode<CharType>();
   }
 
 private:
@@ -120,6 +284,16 @@ private:
 
     m_buffer[string_size_in_chars] = 0;
     m_length = string_size_in_chars;
+  }
+
+  constexpr void _move_right(size_type index, size_type count) {
+    for (size_type i = 0; i < count; ++i, ++index)
+      m_buffer[index + count] = m_buffer[index];
+  }
+
+  constexpr void _move_left(size_type index, size_type count) {
+    for (size_type i = 0; i < count; ++i, ++index)
+      m_buffer[index - count] = m_buffer[index];
   }
 
 private:
